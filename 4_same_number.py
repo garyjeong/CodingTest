1a84d2d5f0ecd4f9542c87f0ebc5abbdd4f62593
garyjeong <smat91@naver.com>
2022-08-22 23:02:11 +0900

    설명 추가 및 기대값/조건

--- 4_same_number.py
+++ 4_same_number.py
@@ -1,9 +1,16 @@
 # 완료
+
+# 기대값 [1,1,3,3,0,1,1] -> [1,3,0,1]
+# 기대값 [4,4,4,3,3] -> [4,3] 
 def solution(arr): 
+    # 단순히 중복제거를 하게 되면 첫번 째 기대값과는 다르게 나온다.
+    # 조건은 단순히 근접한 중복 숫자만 제거하면 된다.
     answer = []
 
-    #
     for i in arr:
+        # answer에 arr의 요소를 넣되 
+        # answer은 처음에 비어있으므로 1개는 넣고 시작
+        # 이후 answer의 마지막 요소와 arr의 가져올 요소를 비교한다.
         if (len(answer) == 0) or (answer[-1] != i):
             answer.append(i)
     return answer
